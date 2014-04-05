6377a2d96db042ddb6bcc3ac862ba7464d186baa
flauro <flauro@users.noreply.github.com>
2014-04-06 09:43:13 +1000

    Update ANI.pl

--- ANI.pl
+++ ANI.pl
@@ -182,23 +182,6 @@ as the results may vary.
 
 -vervose (-v) amount of information displayed (0 = no messages)
 
-=head2 Mailing Lists
-
-User feedback is an integral part of the evolution of this and other
-Bioperl modules. Send your comments and suggestions preferably to
-the Bioperl mailing list.  Your participation is much appreciated.
-
-  bioperl-l@bioperl.org               - General discussion
-  http://bioperl.org/MailList.shtml   - About the mailing lists
-
-=head2 Reporting Bugs
-
-Report bugs to the Bioperl bug tracking system to help us keep track
-the bugs and their resolution.  Bug reports can be submitted via email
-or the web:
-
-  bioperl-bugs@bioperl.org
-  http://bioperl.org/bioperl-bugs/
 
 =head1 AUTHOR - Federico Lauro
 
